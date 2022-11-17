87e44bd714d213b973174f7b73162ae27e5aeaa6
slivcaigor <slivcaigor@gmail.com>
2022-11-17 12:27:54 +0100

    Fix size container

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const selectDifficulty = document.getElementById("select_difficulty");
 const squaresContainer = document.querySelector(".container");
 const startGame = document.getElementById("play");
 
-
 startGame.addEventListener("click", function () {
 
   squaresContainer.innerHTML = "";
@@ -11,6 +10,7 @@ startGame.addEventListener("click", function () {
   const difficulty = selectDifficulty.value;
 
   const totalSquares = numSquares(difficulty);
+
 
   for (let i = 1; i <= totalSquares; i++) {
 
@@ -27,6 +27,7 @@ startGame.addEventListener("click", function () {
       square.classList.toggle("clicked");
       console.log(i);
     });
+
   };
 });
 
@@ -35,14 +36,26 @@ function numSquares(difficulty) {
 
   if (parseInt(difficulty) === 1) {
     let numSquares = 100;
+    squaresContainer.classList.add("container-easy");
+    squaresContainer.classList.remove("container-mid");
+    squaresContainer.classList.remove("container");
+    squaresContainer.classList.remove("container-hard");
     return numSquares;
   }
   if (parseInt(difficulty) === 2) {
     let numSquares = 81;
+    squaresContainer.classList.add("container-mid");
+    squaresContainer.classList.remove("container-easy");
+    squaresContainer.classList.remove("container");
+    squaresContainer.classList.remove("container-hard");
     return numSquares;
   }
   if (parseInt(difficulty) === 3) {
     let numSquares = 49;
+    squaresContainer.classList.add("container-hard");
+    squaresContainer.classList.remove("container-mid");
+    squaresContainer.classList.remove("container");
+    squaresContainer.classList.remove("container-easy");
     return numSquares;
   }
 };
